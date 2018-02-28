e3deadf1391e220f8d8610f9ff12d4658faa4998
zhouhuafei <zhouhuafei@shopex.cn>
2018-02-28 17:39:04 +0800

    module

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,27 @@
 'use strict';
 
-function domCreate(html) {
-    var div = document.createElement('div');
-    div.innerHTML = html;
-    return div.children[0];
-}
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-module.exports = domCreate;
+(function (name, factory) {
+    if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined') {
+        // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // requirejs - amd canon
+        define(factory);
+    } else {
+        // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
+    }
+})('domCreate', function () {
+    function domCreate(html) {
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        return div.children[0];
+    }
+
+    return domCreate;
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-function domCreate(html) {
-    const div = document.createElement('div');
-    div.innerHTML = html;
-    return div.children[0];
-}
+(function (name, factory) {
+    if (typeof exports === 'object' && typeof module !== 'undefined') { // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) { // requirejs - amd canon
+        define(factory);
+    } else { // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
+    }
+})('domCreate', function () {
+    function domCreate(html) {
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        return div.children[0];
+    }
 
-module.exports = domCreate;
+    return domCreate;
+});
