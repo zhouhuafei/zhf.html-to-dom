5adc04fdac345d73e1bbdcb3c89cefdde4bf778a
zhouhuafei <zhouhuafei@shopex.cn>
2018-03-01 14:13:06 +0800

    package

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const htmlToDom = require('../dist/index.min');
 
-test(`创建dom`, () => {
+test(`html转成dom节点`, () => {
     const dom = htmlToDom(`<div>我是被创建的dom</div>`);
     console.log(dom.outerHTML); // <div>我是被创建的dom</div>
     expect(true).toEqual(true);
